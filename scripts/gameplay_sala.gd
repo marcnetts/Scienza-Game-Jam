04007413aa808470ed10da5211f9ca00d0a7fd16
Marcos Zanetti <marcnetts@gmail.com>
2026-07-05 23:25:29 -0300

    Projeto inicial

---
+++ scripts/gameplay_sala.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+@export var tempo_de_jogo: float = 60.0
+@export var horario_label: Label
---
+++ scripts/item_selecionavel.gd
@@ -0,0 +1,71 @@
+extends Area2D
+
+@export var sprite_inicial: Texture2D
+@export var sprite_acao: Texture2D
+@export var nome_acao: String = "limpando" #mandar string ou id de index por ação?
+@export var tempo_limpeza: float = 3.0
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var colisao: CollisionShape2D = $CollisionShape2D
+@onready var barra_progresso: TextureProgressBar = $TextureProgressBar
+
+signal tarefa_concluida
+signal jogador_interagindo(interagindo, acao)
+
+var is_precisa_interagir: bool = false
+var is_mouse_por_cima: bool = false
+var progresso_atual: float = 0.0
+
+func _ready():
+	if sprite_inicial:
+		sprite.texture = sprite_inicial
+	else:
+		sprite_inicial = sprite.texture
+	colisao.shape.size = sprite.texture.get_size()
+	
+	barra_progresso.position.y -= (sprite.texture.get_size().y / 2) + 20
+	barra_progresso.visible = false
+	barra_progresso.max_value = tempo_limpeza
+	
+	sujar() #mandar lógica para a cena depois?
+
+func _process(delta):
+	if is_precisa_interagir and is_mouse_por_cima and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		avancar_limpeza(delta)
+	else:
+		parar_limpeza()
+
+func sujar():
+	is_precisa_interagir = true
+	if sprite_acao:
+		sprite.texture = sprite_acao
+	sprite.modulate = Color(1.5, 1.5, 0.5) #ser glow dps
+
+func avancar_limpeza(delta):
+	barra_progresso.visible = true
+	progresso_atual += delta
+	barra_progresso.value = progresso_atual
+	
+	jogador_interagindo.emit(true, nome_acao) #mudar dps?
+
+	if progresso_atual >= tempo_limpeza:
+		concluir_limpeza()
+
+func parar_limpeza():
+	barra_progresso.visible = false
+	progresso_atual = 0
+	jogador_interagindo.emit(false, "")
+
+func concluir_limpeza():
+	is_precisa_interagir = false
+	progresso_atual = 0.0
+	barra_progresso.visible = false
+	sprite.texture = sprite_inicial
+	sprite.modulate = Color.WHITE #mudar para glow depois
+	tarefa_concluida.emit()
+
+func _on_mouse_entered():
+	is_mouse_por_cima = true
+
+func _on_mouse_exited():
+	is_mouse_por_cima = false
